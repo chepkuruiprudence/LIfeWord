9e2eed9e870d350cbe743e5012b60a55ff290dc9
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-06-24 01:39:39 +0300

    fixed a build deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ function App() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-const getAdvice = async () => {
+const getAdvice = async (e) => {
+  e.preventDefault();
   setLoading (true);
   try {;
   const response = await fetch ('https://api.adviceslip.com/advice');
+  if (!response.ok) {
+    throw new Error("Something went rong");
+  }
   const advice = await response.json();
   setAdvice(advice.slip.advice);
   setLoading(false);
-} catch (error) {
+} catch (e) {
   setAdvice('Failed to fetch advice. Please try again.');
   setLoading(false);
 } finally{
@@ -22,14 +26,14 @@ const getAdvice = async () => {
 };
 
 useEffect(() => {
-  getAdvice();
+  getAdvice({preventDefault: () => {}});
 }, []);
 
   return (
     <>
     <div className='container'>
       <h1>words that count</h1>
-      
+
       {loading ? (<p>Loading ...</p>) : (<textarea value={advice} error = {error} id="adviceTxt" readOnly>  {advice}</textarea>)}
       
       <button className='getBtn' onClick={getAdvice}>get advice</button>
