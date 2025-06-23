c39eb6d753927328e5a33cfd3d10aba33b638804
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-06-24 00:54:28 +0300

    its an app that fetches advices from an API

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [advice, setAdvice] = useState('Fetching advice ...');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+const getAdvice = async () => {
+  setLoading (true);
+  try {;
+  const response = await fetch ('https://api.adviceslip.com/advice');
+  const advice = await response.json();
+  setAdvice(advice.slip.advice);
+  setLoading(false);
+} catch (error) {
+  setAdvice('Failed to fetch advice. Please try again.');
+  setLoading(false);
+} finally{
+ setLoading(false);
+}
+};
+
+useEffect(() => {
+  getAdvice();
+}, []);
+
+  return (
+    <>
+    <div className='container'>
+      <h1>words that count</h1>
+      
+      {loading ? (<p>Loading ...</p>) : (<textarea value={advice} error = {error} id="adviceTxt" readOnly>  {advice}</textarea>)}
+      
+      <button className='getBtn' onClick={getAdvice}>get advice</button>
+    </div> 
+    </>
+  )
+}
+
+export default App;
